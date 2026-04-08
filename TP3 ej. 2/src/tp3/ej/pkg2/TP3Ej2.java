4988cc4c25bcb5d99c0e71ff04356950c6d6a6a3
brianticona <bri.ticonam@gmail.com>
2026-04-07 21:50:05 -0300

    cambio de 0 a 1

--- TP3 ej. 2/src/tp3/ej/pkg2/TP3Ej2.java
+++ TP3 ej. 2/src/tp3/ej/pkg2/TP3Ej2.java
@@ -4,19 +4,19 @@ public class TP3Ej2 {
     public static void main(String[] args) {
         Scanner na = new Scanner(System.in);
         int n = na.nextInt();
-        int contador1 = 0;
+        int contador1 = 1;
         while(contador1 <= n){
             System.out.println("Contador 1 = " + contador1);
             contador1++;
         }
         System.out.println("-----------------");
-        int contador2 = 0;
+        int contador2 = 1;
         do {            
             System.out.println("contador 2 = " + contador2);
             contador2++;
         } while (contador2 <= n);
         System.out.println("-----------------");
-        for(int c = 0; c <= n; c ++){
+        for(int c = 1; c <= n; c ++){
             System.out.println("contador 3 = " + c);
         }
     }
