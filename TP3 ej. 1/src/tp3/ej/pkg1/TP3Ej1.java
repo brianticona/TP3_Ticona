f227c26e5582f925af5e7b630348d5a27147fc0e
brianticona <bri.ticonam@gmail.com>
2026-04-07 21:50:58 -0300

    Update TP3Ej1.java

--- TP3 ej. 1/src/tp3/ej/pkg1/TP3Ej1.java
+++ TP3 ej. 1/src/tp3/ej/pkg1/TP3Ej1.java
@@ -1,19 +1,19 @@
 package tp3.ej.pkg1;
 public class TP3Ej1 {
     public static void main(String[] args) {
-        int contador1 = 0;
+        int contador1 = 1;
         while(contador1 <= 100){
             System.out.println("Contador 1 = " + contador1);
             contador1++;
         }
         System.out.println("-----------------");
-        int contador2 = 0;
+        int contador2 = 1;
         do {            
             System.out.println("contador 2 = " + contador2);
             contador2++;
         } while (contador2 <= 100);
         System.out.println("-----------------");
-        for(int c = 0; c <= 100; c ++){
+        for(int c = 1; c <= 100; c ++){
             System.out.println("contador 3 = " + c);
         }
     }
