b217a31853e31ed552858b0883ecca5a70e972f6
brianticona <bri.ticonam@gmail.com>
2026-04-11 19:57:09 -0300

    .

--- TP3 eje. 11/src/tp3/eje/pkg11/TP3Eje11.java
+++ TP3 eje. 11/src/tp3/eje/pkg11/TP3Eje11.java
@@ -1,6 +1,7 @@
 package tp3.eje.pkg11;
 public class TP3Eje11 {
     public static void main(String[] args) {
+        System.out.println("Todos los numeros del 100 al 0 con decrementos de 23.");
         for (int i = 100; i >= 0; i -= 23){
             System.out.println(i);
         }
