92301d62ae5d9b10ee07e45a386e59801e33ab09
brianticona <bri.ticonam@gmail.com>
2026-04-11 19:26:27 -0300

    ej11

---
+++ TP3 eje. 11/src/tp3/eje/pkg11/TP3Eje11.java
@@ -0,0 +1,8 @@
+package tp3.eje.pkg11;
+public class TP3Eje11 {
+    public static void main(String[] args) {
+        for (int i = 100; i >= 0; i -= 23){
+            System.out.println(i);
+        }
+    }
+}
